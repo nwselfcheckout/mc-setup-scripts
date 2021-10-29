ae9f139c1cf0873755c08ecdc0a519f363f88560
mosguinz <mos.guinz@gmail.com>
2021-10-29 07:49:28 -0700

    Add envars format

---
+++ 2-set-envars.sh
@@ -0,0 +1,9 @@
+# Env variable names for reference, don't run this.
+
+export MC_RCON_PORT=1234
+export MC_RCON_PASSWORD=password
+
+# Deprecated, just use "localhost" on places where this is referenced.
+
+export MC_HOST_ADDRESS=host.tld
+
