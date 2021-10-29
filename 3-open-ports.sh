e4e62fed1ea6861026473c645463b64e893fb71e
mosguinz <mos.guinz@gmail.com>
2021-10-29 08:10:24 -0700

    Add documentation for port opener

--- 3-open-ports.sh
+++ 3-open-ports.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+usage () {
+	echo "usage: $0 [port ...]"
+	echo "    Open the provided PORTs for TCP and UDP traffic."
+	echo "    Typically one for the game (query) server and one for RCON requests."
+	exit
+}
+
+if [ $# == 0 ]; then
+	usage
+fi
+
 for port in "$@"
 do
 	sudo firewall-cmd --permanent --zone=public --add-port=$port/tcp;
