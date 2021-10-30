198968b4104cd8cb088baecfcff066648cc55a88
mosguinz <mos.guinz@gmail.com>
2021-10-30 03:54:07 -0700

    Prevent set-envars from running

--- 2-set-envars.sh
+++ 2-set-envars.sh
@@ -1,3 +1,9 @@
+echo "Do not run me! Instead, set the environment variables yourself."
+echo "Example:"
+echo "export MC_RCON_PORT=1234"
+echo "export MC_RCON_PASSWORD=password"
+exit
+
 # Env variable names for reference, don't run this.
 
 export MC_RCON_PORT=1234
